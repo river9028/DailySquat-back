67bcd38964acc17c1fb58d0e09de172c5c00185b
jaemin-jeong <jaemin-jeong@naver.com>
2020-04-11 23:39:09 +0900

    add saveCount, getCount func

--- server/controllers/count.js
+++ server/controllers/count.js
@@ -1,14 +1,27 @@
+const jwt = require('jsonwebtoken');
 const count = require('../models/count');
+const secretKey = require('../config/jwt');
 
 module.exports = {
   count: {
-    // 토큰, 유저 아이디, 운동 아이디, 카운트 횟수
-    saveCount: async (req, res) => {
-      const result = await count.count.insertCount(req.body);
+    // 유저 아이디, 운동 아이디, 카운트 횟수
+    addCount: async (req, res) => {
+      const result = await count.count.save(req.body);
       res.send(result);
     },
+    // 1.헤더에 토큰, 2.바디에 운동 아이디를 담아 요청해야됨
     getCount: async (req, res) => {
-
+      const token = req.get('accessToken');
+      // 토큰 확인
+      if (token) {
+        const decoded = jwt.verify(JSON.parse(token), secretKey.key);
+        const idObj = {
+          userId: decoded.id,
+          categoryId: req.body.id,
+        };
+        const result = await count.count.get(idObj);
+        res.send(JSON.stringify({ totalCount: result }));
+      }
     },
   },
 };
--- server/routes/count.js
+++ server/routes/count.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const controller = require('../controllers/count');
 
-router.post('/saveCount', controller.count.saveCount);
+router.post('/saveCount', controller.count.addCount);
+
 router.post('/getCount', controller.count.getCount);
 
 module.exports = router;
