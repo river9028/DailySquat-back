bc8984eee5529a46d36cfb149529eb815e163a91
jaemin-jeong <jaemin-jeong@naver.com>
2020-04-13 03:15:26 +0900

    add crypto

--- server/models/users.js
+++ server/models/users.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const db = require('../db/models/index.js');
 
 module.exports = {
@@ -16,6 +17,13 @@ module.exports = {
         },
       }).spread((user, created) => {
         if (created) {
+          const buff = Buffer.alloc(16, user.dataValues.id);
+          const salt = crypto.randomFillSync(buff).toString('hex');
+          const pwd = crypto.scryptSync(user.dataValues.password, salt, 64).toString('hex');
+          console.log('회원가입할 때 salt는 : ', salt);
+          console.log('회원가입할 때 pwd는 : ', pwd);
+          db.users.update({ st: salt, password: pwd }, { where: { email: user.dataValues.email } })
+            .catch((err) => err);
           result = 'signup success';
         } else {
           result = 'exist email';
@@ -27,10 +35,13 @@ module.exports = {
 
     // 로그인
     signin(body) {
-      return db.users.findOne({ where: { email: body.email, password: body.password, status: 'N' } })
+      return db.users.findOne({ where: { email: body.email, status: 'N' } })
         .then((data) => {
           let result;
-          if (data) {
+          const salt = data.dataValues.st;
+          const pwd = crypto.scryptSync(body.password, salt, 64).toString('hex');
+          console.log('로그인 할 때 암호화된 pwd는 : ', pwd);
+          if (data.dataValues.password === pwd) {
             result = { message: 'login success', id: JSON.stringify(data.id) };
           } else {
             result = { message: 'login fail', id: null };
