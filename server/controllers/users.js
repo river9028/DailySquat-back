899e609676e2e02b14f2cc6deb5e0801ce192d33
jaemin-jeong <jaemin-jeong@naver.com>
2020-04-13 18:19:10 +0900

    add user info api

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -43,7 +43,30 @@ module.exports = {
         res.send(err);
       }
     },
-    info: (/* req, res */) => { },
+    info: async (req, res) => {
+      try {
+        const token = req.get('accessToken');
+        if (token) {
+          const decoded = jwt.verify(JSON.parse(token), secretKey.key);
+          const result = await users.users.info(decoded);
+          const year = result.createdAt.getFullYear();
+          const month = result.createdAt.getMonth() + 1;
+          const date = result.createdAt.getDate();
+          const infoData = {
+            name: result.name,
+            email: result.email,
+            age: result.age,
+            gender: result.gender,
+            createdAt: `${year}/${month}/${date}`,
+          };
+          res.send(JSON.stringify(infoData));
+        } else {
+          res.sendStatus(403);
+        }
+      } catch (err) {
+        res.send(err);
+      }
+    },
     isDuplicate: async (req, res) => {
       // console.log(req.params);
       const result = await users.users.isDuplicate(req.params);
